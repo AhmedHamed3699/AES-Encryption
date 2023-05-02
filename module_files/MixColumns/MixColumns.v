e06ccd180fe36a0fcefaae8b4b0668357ecd4c77
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-05-02 05:14:38 +0300

    fixed some bugs and comiled successfully

--- module_files/MixColumns/MixColumns.v
+++ module_files/MixColumns/MixColumns.v
@@ -1,47 +1,47 @@
 
 module  MixColumns(state, result_state);
 
-input [7:0] state [0:3][0:3];
-output [7:0] result_state [0:3][0:3];
+input [127:0] state;
+output [127:0] result_state;
 
-function [7:0] multiply;
+function [7:0] mult;
     input [1:0] a;
     input [7:0] b;
     begin
         case(a)
 
             2'b10: begin
-                if(multiply[7] == 1)
-                    multiply = (b << 1) ^ 8'h1b;
+                if(mult[7] == 1)
+                    mult = (b << 1) ^ 8'h1b;
                 else
-                    multiply = b << 1;
+                    mult = b << 1;
             end
 
             2'b11: begin
 
-                if(multiply[7] == 1)
-                    multiply = (b << 1) ^ 8'h1b;
+                if(mult[7] == 1)
+                    mult = (b << 1) ^ 8'h1b;
                 else
-                    multiply = b << 1;
+                    mult = b << 1;
 
-                multiply = multiply ^ b;
+                mult = mult ^ b;
 
             end
 
-            default: multiply = multiply;
+            default: mult = mult;
 
         endcase
-    end 
-    
+    end
 endfunction
-genvar j;
+
+genvar i;
 generate
-    
-for (j = 0; j < 4; j = j + 1) begin
-    result_state[0][j] = multiply(.a(2'b10), .b(state[0][j])) ^ multiply(.a(2'b11), .b(state[1][j])) ^ state[2][j] ^ state[3][j];
-    result_state[1][j] = state[0][j] ^ multiply(.a(2'b10), .b(state[1][j])) ^ multiply(.a(2'b11), .b(state[2][j])) ^ state[3][j];
-    result_state[2][j] = state[0][j] ^ state[1][j] ^ multiply(.a(2'b10), .b(state[2][j])) ^ multiply(.a(2'b11), .b(state[3][j]));
-    result_state[3][j] = multiply(.a(2'b11), .b(state[0][j])) ^ state[1][j] ^ state[2][j] ^ multiply(.a(2'b10), .b(state[3][j]));
+
+for (i = 0; i < 4; i = i + 1) begin
+    assign result_state[(i*32)+:8] = mult(2'b10, state[(i*32)+:8]) ^ mult(2'b11, state[((i*32)+8)+:8]) ^ state[((i*32)+16)+:8] ^ state[((i*32)+16)+:8];
+    assign result_state[((i*32)+8)+:8] = state[(i*32)+:8] ^ mult(2'b10, state[((i*32)+8)+:8]) ^ mult(2'b11, state[((i*32)+16)+:8]) ^ state[((i*32)+16)+:8];
+    assign result_state[((i*32)+16)+:8] = state[(i*32)+:8] ^ state[((i*32)+8)+:8] ^ mult(2'b10, state[((i*32)+16)+:8]) ^ mult(2'b11, state[((i*32)+16)+:8]);
+    assign result_state[((i*32)+16)+:8] = mult(2'b11, state[(i*32)+:8]) ^ state[((i*32)+8)+:8] ^ state[((i*32)+16)+:8] ^ mult(2'b10, state[((i*32)+16)+:8]);
 end
 
 endgenerate
