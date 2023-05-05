a303688be6e16c3b4ab65ee7673c9df24f327427
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-05-06 02:06:21 +0300

    clean up

--- module_files/ShiftRows/ShiftRows.v
+++ module_files/ShiftRows/ShiftRows.v
@@ -3,39 +3,74 @@ input [127:0] state,
 output [127:0] out    
 );
 
-//2d arrays of rows=4 , cols=4
-wire [7:0]stateMatrix[3:0][3:0];
-wire [7:0]outMatrix[3:0][3:0];
+wire [7:0]b0;
+wire [7:0]b1;
+wire [7:0]b2;
+wire [7:0]b3;
 
-genvar i,j;
+wire [7:0]b4;
+wire [7:0]b5;
+wire [7:0]b6;
+wire [7:0]b7;
 
-//changing state into matrix
-for(i=3; i>=0 ; i=i-1)begin
-    for(j=3; j>=0 ; j=j-1)begin
-        assign stateMatrix[3-j][3-i]=state[8*j+32*i+7 : 8*j+32*i];
-    end
-end
+wire [7:0]b8;
+wire [7:0]b9;
+wire [7:0]b10;
+wire [7:0]b11;
 
-/*
-//shift left function function
-function [31:0]shiftLeft;
-    input [31:0]in;
-    shiftLeft={in[23:0] ,in[31:24]};
-endfunction
-*/
+wire [7:0]b12;
+wire [7:0]b13;
+wire [7:0]b14;
+wire [7:0]b15;
 
-//shifting rows by calling function
-for(i=0 ; i<4 ; i=i+1) begin
-    for(j=i ; j>=0 ; j=j-1)begin
-        assign outMatrix[i][3:0]={stateMatrix[i][0][7:0] , stateMatrix[i][3][7:0]  ,stateMatrix[i][2][7:0]  , stateMatrix[i][1][7:0] };
-    end
-end
+//copying col1
+assign b0=state[127-(0*8)-:8];
+assign b1=state[127-(1*8)-:8];
+assign b2=state[127-(2*8)-:8];
+assign b3=state[127-(3*8)-:8];
 
-//retruning matrix into array
-for(i=3 ; i>=0 ; i=i-1)begin
-    for(j=3; j>=0 ; j=j-1)begin
-   assign out[8*j+32*i+7 : 8*j+32*i]=outMatrix[3-j][3-i];
-    end    
-end
+//copying col2
+assign b4=state[95-(0*8)-:8];
+assign b5=state[95-(1*8)-:8];
+assign b6=state[95-(2*8)-:8];
+assign b7=state[95-(3*8)-:8];
+
+//copying col3
+assign b8=state[63-(0*8)-:8];
+assign b9=state[63-(1*8)-:8];
+assign b10=state[63-(2*8)-:8];
+assign b11=state[63-(3*8)-:8];
+
+//copying col4
+assign b12=state[31-(0*8)-:8];
+assign b13=state[31-(1*8)-:8];
+assign b14=state[31-(2*8)-:8];
+assign b15=state[31-(3*8)-:8];
+
+//setting out
+
+//col1
+assign out[127-(0*8)-:8]=b0;
+assign out[127-(1*8)-:8]=b5;
+assign out[127-(2*8)-:8]=b10;
+assign out[127-(3*8)-:8]=b15;
+
+//col2
+assign out[95-(0*8)-:8]=b4;
+assign out[95-(1*8)-:8]=b9;
+assign out[95-(2*8)-:8]=b14;
+assign out[95-(3*8)-:8]=b3;
+
+//col3
+assign out[63-(0*8)-:8]=b8;
+assign out[63-(1*8)-:8]=b13;
+assign out[63-(2*8)-:8]=b2;
+assign out[63-(3*8)-:8]=b7;
+
+//col4
+assign out[31-(0*8)-:8]=b12;
+assign out[31-(1*8)-:8]=b1;
+assign out[31-(2*8)-:8]=b6;
+assign out[31-(3*8)-:8]=b11;
 
 endmodule
