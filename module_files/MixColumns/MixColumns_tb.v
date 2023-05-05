57388bc5089f3d9f33980a4954c93cf8e0152648
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-05-06 01:30:44 +0300

    fixed bugs and tested properly

---
+++ module_files/MixColumns/MixColumns_tb.v
@@ -0,0 +1,22 @@
+
+`include "MixColumns.v"
+module MixColumns_tb ();
+    
+    reg [127:0] Data_in;
+    wire [127:0] Data_out;
+
+    MixColumns m(.state(Data_in), .result_state(Data_out));
+
+    initial begin
+        // $display("Data_in\t\tData_out");
+        // $monitor("%h %h", Data_in, Data_out);
+        Data_in= 128'h_d4bf5d30_e0b452ae_b84111f1_1e2798e5;
+        #20
+        if(Data_out == 128'h_046681e5_e0cb199a_48f8d37a_2806264c)
+            $display("Passed");
+        else
+            $display("Failed");
+        #10;
+    end
+
+endmodule
