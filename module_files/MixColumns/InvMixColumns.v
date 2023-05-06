711f6d62fecc46088e16570b2122f2c575107503
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-05-06 03:48:27 +0300

    attempt to test

--- module_files/MixColumns/InvMixColumns.v
+++ module_files/MixColumns/InvMixColumns.v
@@ -4,9 +4,17 @@ module  InvMixColumns(
     output [0:127] result_state
 );
 
-function [7:0] mult (input [1:0] a, input [7:0] b);
+function [7:0] mult (input [3:0] a, input [7:0] b);
+    reg [10:0] temp1;
+    reg [10:0] temp2;
+    reg [10:0] temp3;
+    reg [10:0] temp4;
     begin
-
+        temp1[7:0]  = a[0] & b;
+        temp2[8:1]  = a[1] & b;
+        temp3[9:2]  = a[2] & b;
+        temp4[10:3] = a[3] & b;
+        mult = temp1 + temp2 + temp3 + temp4;
     end
 endfunction
 
