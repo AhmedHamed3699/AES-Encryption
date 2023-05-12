072f9d38571306a7b5ba03d176561fc18899a571
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-05-12 20:50:16 +0300

    modifed subBytes

--- module_files/subBytes/SubBytes.v
+++ module_files/subBytes/SubBytes.v
@@ -1,11 +1,11 @@
 `include "SBox.v"
-module SubBytes #(parameter n = 128)(byte,sub_byte);
-input [n-1:0] byte;
-output [n-1:0] sub_byte;
+module SubBytes (byte,sub_byte);
+input [127:0] byte;
+output [127:0] sub_byte;
 
 genvar i;
 generate 
-for(i = 0; i < n; i = i+8)  
+for(i = 0; i < 128; i = i+8)  
 	SBox s(byte[i +:8], sub_byte[i +:8]);
 endgenerate
 
