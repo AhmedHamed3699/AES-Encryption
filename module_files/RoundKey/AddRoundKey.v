66366c8ef060ced239a78a2e32f8622912b636e4
Samy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2023-05-14 16:18:30 +0300

    encryption done

--- module_files/RoundKey/AddRoundKey.v
+++ module_files/RoundKey/AddRoundKey.v
@@ -4,10 +4,7 @@ module AddRoundKey (
     output [127:0]state_out
 );
 
-genvar i;
 
-for(i=127 ; i>=0 ; i=i-1)begin
-   assign state_out[i]=state_in[i]^RoundKey[i];
-end
+   assign state_out=state_in^RoundKey;
     
 endmodule
