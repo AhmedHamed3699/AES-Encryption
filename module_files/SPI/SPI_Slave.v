068b11c94777370bfa9eb4854abd6bffd2f63795
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-05-14 16:38:26 +0300

    created functionalit for slave recieve

--- module_files/SPI/SPI_Slave.v
+++ module_files/SPI/SPI_Slave.v
@@ -13,17 +13,35 @@ reg [127:0] data_in;
 // output consists of 128 bits of encrypted data
 reg [127:0] data_out;
 
+integer i = 0;
+
 always @(posedge clk, posedge rst) begin
     if(rst) begin
         data_in <= 0;
         data_out <= 0;
+        key <= 0;
+        i <= 0;
     end
     else begin
         if(!CS) begin
-            data_in <= {data_in[127:0], SDI};
+            if(i < 128)begin
+                data_in <= {data_in[127:0], SDI};
+                i <= i + 1;
+            end
+            else if(i < (128 + (32*Nk))) begin
+                key <= {key[(32*Nk)-1:0], SDI};
+                i <= i + 1;
+            end
+            else begin
+                data_out <= data_in;
+                data_in <= {data_in[127:0], SDI};
+            end
         end
         else begin
             data_in <= 0;
+            data_out <= 0;
+            key <= 0;
+            i <= 0;
         end
     end
 end
