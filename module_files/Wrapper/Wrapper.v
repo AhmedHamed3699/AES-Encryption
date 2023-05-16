cdb50d95e13371113c64cb955972a42284346d81
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-05-16 11:44:23 +0300

    done

--- module_files/Wrapper/Wrapper.v
+++ module_files/Wrapper/Wrapper.v
@@ -5,59 +5,58 @@ module Wrapper #(parameter Nk =4 , parameter Nr=10)
 (
     input clk,
     input reset,
-    input [127:0] data_in_encrypt,
+    input [127:0] data_in,
     input [Nk*32-1:0] key_in,
     output reg wrapper_out_encrypt,
     output reg wrapper_out_decrypt
 );
 
-SPI_Master #(Nk,Nr) SM(clk , reset , data_in_encrypt , key_in , done_out_Enc , done_out_Dec , data_out);
+wire done_out_Enc;
+wire done_out_Dec;
+reg done_reg;
+wire [127:0] data_out;
+reg [127:0] data;
 
+SPI_Master #(Nk,Nr) SM(clk , reset , data , key_in , done_out_Enc , done_out_Dec , data_out);
+
+always @(data_in) begin
+    data <= data_in;
+end
 
 always@*
 begin
-    if (Nk == 4) begin
-        if(data_encrypted == 128'h69c4e0d86a7b0430d8cdb78070b4c55a)
-            wrapper_out_encrypt<= 1'b1;
-        else
-            wrapper_out_encrypt<= 1'b0;
-    end
-    else if (Nk == 6) begin
-        if(data_encrypted == 128'hdda97ca4864cdfe06eaf70a0ec0d7191)
-            wrapper_out_encrypt<= 1'b1;
-        else
-            wrapper_out_encrypt<= 1'b0;
-    end
-    else begin
-        if(data_encrypted == 128'h8ea2b7ca516745bfeafc49904b496089)
-            wrapper_out_encrypt<= 1'b1;
-        else
-            wrapper_out_encrypt<= 1'b0;
-    end
+  if(done_out_Enc)begin
+  if (Nk == 4) begin
+      if(data_out == 128'h69c4e0d86a7b0430d8cdb78070b4c55a)
+          wrapper_out_encrypt<= 1'b1;
+      else
+          wrapper_out_encrypt<= 1'b0;
+      data = 128'h69c4e0d86a7b0430d8cdb78070b4c55a;
+  end
+  else if (Nk == 6) begin
+      if(data_out == 128'hdda97ca4864cdfe06eaf70a0ec0d7191)
+          wrapper_out_encrypt<= 1'b1;
+      else
+          wrapper_out_encrypt<= 1'b0;
+      data = 128'hdda97ca4864cdfe06eaf70a0ec0d7191;
+  end
+  else begin
+      if(data_out == 128'h8ea2b7ca516745bfeafc49904b496089)
+          wrapper_out_encrypt<= 1'b1;
+      else
+          wrapper_out_encrypt<= 1'b0;
+      data = 128'h8ea2b7ca516745bfeafc49904b496089;
+  end
 
-    if(data_decrypted == 128'h00112233445566778899aabbccddeeff)
-    wrapper_out_decrypt<= 1'b1;
-    else
-    wrapper_out_decrypt<=1'b0;
+  end
+    if (done_out_Dec) begin
+      if(data_out == 128'h00112233445566778899aabbccddeeff)
+          wrapper_out_decrypt<= 1'b1;
+      else
+          wrapper_out_decrypt<=1'b0;
+    end
 end
 
-always @(*) begin
-  if(done_out_Enc)begin
-      if(data_out == 128'h8ea2b7ca516745bfeafc49904b496089)
-        $display("successfully encrypted");
-      else
-        $display("failed encryption");
-
-      data_in = 128'h8ea2b7ca516745bfeafc49904b496089;
-  end
-  if (done_out_Dec) begin
-
-      if(data_out == 128'h00112233445566778899aabbccddeeff)
-        $display("successfully decrypted");
-      else
-        $display("failed decryption");
-      $finish;  
-  end
-  end
+assign done = done_reg;
 
 endmodule
--- module_files/Wrapper/Wrapper_tb.v
+++ module_files/Wrapper/Wrapper_tb.v
@@ -6,13 +6,20 @@ parameter Nk=4;
 parameter Nr=Nk+6;
 reg [127:0] data_in;
 reg [Nk*32-1:0] key_in;
+reg clk;
+reg reset;
 wire wrapper_out_encrypt;
 wire wrapper_out_decrypt;
-Wrapper  #(Nk,Nr) wrab(data_in,key_in,wrapper_out_encrypt,wrapper_out_decrypt);
+Wrapper  #(Nk,Nr) wrab(clk, reset, data_in ,key_in ,wrapper_out_encrypt ,wrapper_out_decrypt);
 
+
+always @(*) begin
+    #5  clk <= ~clk;
+end
 
 initial begin
-
+clk = 0;
+reset=1;
 data_in=128'h00112233445566778899aabbccddeeff;
 
 key_in=128'h000102030405060708090a0b0c0d0e0f;  // 128'h69c4e0d86a7b0430d8cdb78070b4c55a
@@ -20,16 +27,20 @@ key_in=128'h000102030405060708090a0b0c0d0e0f;  // 128'h69c4e0d86a7b0430d8cdb7807
 //key_in=256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;  // 128'h8ea2b7ca516745bfeafc49904b496089
 
 
-#10
-if(wrapper_out_encrypt == 1'b1)
-$display("Data Successfully Encrypted");
-else
-$display("Encryption Failedddddddd");
-#10
-if(wrapper_out_decrypt == 1'b1)
-$display("Data Successfully Decrypted");
-else
-$display("Decryption Failedddddddd");
+#20
+reset = 0;
+#10000
+
+    if(wrapper_out_encrypt == 1'b1)
+    $display("Data Successfully Encrypted");
+    else
+    $display("Encryption Failed");
+    if(wrapper_out_decrypt == 1'b1)
+    $display("Data Successfully Decrypted");
+    else
+    $display("Decryption Failed");
+
+$finish; 
 
 
 end
